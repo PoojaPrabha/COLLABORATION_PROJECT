ca0b25fbde5acca5da15bd8e6973991d421ede03
Pooja <pooja@mail>
2018-02-17 16:34:39 +0530

    job module

--- collaboration_backend/src/main/java/com/MYPROJ/configuration/DBConfiguration.java
+++ collaboration_backend/src/main/java/com/MYPROJ/configuration/DBConfiguration.java
@@ -12,12 +12,13 @@ import org.springframework.orm.hibernate4.HibernateTransactionManager;
 import org.springframework.orm.hibernate4.LocalSessionFactoryBuilder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-
 import com.MYPROJ.model.User;
+import com.MYPROJ.model.Job;
 
 @Configuration
 @EnableTransactionManagement
 public class DBConfiguration {
+	
 	public DBConfiguration() {
 		System.out.println("DBConfiguration class INSTANTIATED");
 	}
@@ -30,7 +31,7 @@ public class DBConfiguration {
 		hibernateProperties.setProperty("hibernate.hbm2ddl.auto", "update");
 		hibernateProperties.setProperty("hibernate.show_sql", "true");
 		lsf.addProperties(hibernateProperties);
-		Class classes[] = new Class[] {User.class};
+		Class classes[] = new Class[] {User.class,Job.class};
 		return lsf.addAnnotatedClasses(classes).buildSessionFactory();
 	}
 
